f78c6d8140f16b3d627c5b3757757fc632b55263
Crisnaldo Carvalho Santos <45441190+CrisnaldoSantos@users.noreply.github.com>
2021-04-10 11:06:52 -0300

    feat: added jwtStrategy and linked in tasks module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { AuthService } from './shared/auth.service';
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './shared/local.strategy';
+import { JwtStrategy } from './shared/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from './shared/constants';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '7200s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/shared/auth.service.ts
+++ src/auth/shared/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/shared/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(userEmail: string, userPassword: string) {
     const user = await this.usersService.getByEmail(userEmail);
@@ -12,5 +16,12 @@ export class AuthService {
       return { id: _id, name, email };
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.eamil, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
