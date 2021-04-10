d45ff1e2e6821da003c170307fd47d9584bf2d1a
Crisnaldo Carvalho Santos <45441190+CrisnaldoSantos@users.noreply.github.com>
2021-04-10 09:49:28 -0300

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/teste'),
     TasksModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
