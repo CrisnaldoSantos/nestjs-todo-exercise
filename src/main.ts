85c5187ea4986a8852ba6e30770a5163f9442a9c
Crisnaldo Carvalho Santos <45441190+CrisnaldoSantos@users.noreply.github.com>
2021-04-10 14:14:06 -0300

    feat: add initial documentation with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS API Exercise')
+    .setDescription(
+      'Aplicação simples de TODO list com users e controle de usuários para estudo do NestJs e sua implementação modular.',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
